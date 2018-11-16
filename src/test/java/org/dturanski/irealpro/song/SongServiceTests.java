34d7062e501b0610ca4f7615411d23ab37d9efbc
David Turanski <dturanski@pivotal.io>
2018-11-16 15:02:31 -0500

    Start support for multiple matches

--- src/test/java/org/dturanski/irealpro/song/SongServiceTests.java
+++ src/test/java/org/dturanski/irealpro/song/SongServiceTests.java
@@ -17,8 +17,12 @@
 package org.dturanski.irealpro.song;
 
 import java.util.List;
+import java.util.function.BiPredicate;
 
+import org.apache.commons.text.similarity.LevenshteinDetailedDistance;
+import org.apache.commons.text.similarity.LevenshteinResults;
 import org.dturanski.irealpro.song.domain.SongEntity;
+import org.dturanski.irealpro.song.repository.SongRepository;
 import org.dturanski.irealpro.song.service.SongService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +43,9 @@ public class SongServiceTests {
 	@Autowired
 	private SongService songService;
 
+	@Autowired
+	private SongRepository songRepository;
+
 	@Test
 	public void basicSearch() {
 		List<SongEntity> songs = songService.searchSongsByTitle("Love");
@@ -50,4 +57,49 @@ public class SongServiceTests {
 		songs = songService.searchSongsByTitle("The One I Love Belongs To Somebody Else");
 		assertThat(songs.size()).isOne();
 	}
+
+	@Test
+	public void sandbox() {
+
+		Iterable<SongEntity> songs = songRepository.findallOriginalSongs();
+
+		matchTitle(songs, "Love");
+
+		matchTitle(songs, "The One I Love Belongs To Somebody Else");
+
+		matchTitle(songs, "Three O'Clock In the Morning");
+
+		matchTitle(songs, "Heartaches");
+
+		matchTitle(songs, "Blue Skies");
+
+		matchTitle(songs, "Green Eyes");
+
+		matchTitle(songs, "Green");
+
+		matchTitle(songs, "Do You Know What It Means To Miss New Orleans");
+
+		matchTitle(songs, "I'm Gonna Sit Right Down And Taking A Chance");
+	}
+
+	private void matchTitle(Iterable<SongEntity> songs, String title) {
+		songs.forEach(s -> {
+			if (match.test(s.getTitle(), title)) {
+				System.out.println("\n ******** possible Match!!!!\n");
+			}
+		});
+	}
+
+	BiPredicate<String, String> match = (s1, s2) -> {
+		int deltaLen = Math.abs(s1.length() - s2.length());
+		int maxLen = Math.max(s1.length(), s2.length());
+		LevenshteinResults results = LevenshteinDetailedDistance.getDefaultInstance().apply(s1, s2);
+		int dist = results.getDistance();
+		int subs = results.getSubstituteCount();
+		double ratio = dist * 1.0 / maxLen;
+		System.out.println(
+			String.format("%s->%s delta_len: %d  dist: %d subs: %d ratio %f", s2, s1, deltaLen, dist, subs,
+				ratio));
+		return (subs == 0 || dist <= deltaLen || ratio < .5);
+	};
 }
